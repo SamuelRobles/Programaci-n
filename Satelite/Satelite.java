f1117eb0b69ac206b3989b90e3f0e6658c41b85f
usuario <samuel.roblesrivas@nervion.salesianas.org>
2019-10-22 14:06:03 +0200

    Cambios en satelite

--- Satelite/Satelite.java
+++ Satelite/Satelite.java
@@ -55,13 +55,17 @@ public class Satelite {
 				" merididano "+meridiano+" y a la distancia de la tierra de "
 				+distancia_tierra);
 	}
+	
+}
+
+public class Central{
 	//Main
-	public static void main(String[] args) {
-		Satelite s1, s2;
-		s1=new Satelite();
-		s2=new Satelite(4, 2, 9);
-		s1.setPosicion(5, 7, 3);
-		s1.printPosicion();
-		s2.printPosicion();
-	}
+		public static void main(String[] args) {
+			Satelite s1, s2;
+			s1=new Satelite();
+			s2=new Satelite(4, 2, 9);
+			s1.setPosicion(5, 7, 3);
+			s1.printPosicion();
+			s2.printPosicion();
+		}
 }

7c2fcbfde8bba702608f455a15e78cd144fc0287
usuario <samuel.roblesrivas@nervion.salesianas.org>
2019-10-22 13:49:33 +0200

    Satélites

--- Satelite/Satelite.java
+++ Satelite/Satelite.java
@@ -1,11 +1,14 @@
 package Satelite;
 
+//Creamos una clase
 public class Satelite {
 	
+	//Creamos atributos privados
 	private double meridiano;
 	private double paralelo;
 	private double distancia_tierra;
 	
+	//Constructores
 	Satelite(){
 		meridiano=paralelo=distancia_tierra=0;
 	}
@@ -15,8 +18,17 @@ public class Satelite {
 		paralelo=p;
 		distancia_tierra=d;
 	}
+	
+	//Metodos
 	public void setPosicion(double m, double p, double d) {
-		
+		meridiano=m;
+		paralelo=p;
+		distancia_tierra=d;
 	}
-
+	
+	public void print(double m, double p, double d) {
+		System.out.println("El parelelo se encuentra en el paralero "+paralelo+
+				" merididano "+meridiano+" y a la distancia de la tierra de "
+				+distancia_tierra);
+	}
 }
